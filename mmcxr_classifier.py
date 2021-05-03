b7d485f6ac7b47eef98816af2755d1a4095f6415
John Anderson Garcia <jagh1729@gmail.com>
2021-05-03 17:09:57 +0200

    Added classifier for mimic-cxr

--- mmcxr_classifier.py
+++ mmcxr_classifier.py
@@ -3,9 +3,39 @@
 import os
 from glob import glob
 import pandas as pd
+import collections
+import matplotlib.pyplot as plt
 
 from engine.utils import Utils
 from engine.preprocessing import MIMICCXR
+from engine.dnn_classifier import DenseNET121
+
+
+
+def displayLearningCurves(history):
+    ## Saving the convergence curves
+    # print(history.history.keys())
+    ## Accuracy plot
+    plt.figure(1)
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.savefig(os.path.join(dir_dnn_train, "accuracy.png"))
+    plt.close()
+
+    ## Loss plot
+    plt.figure(2)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.savefig(os.path.join(dir_dnn_train, "loss.png"))
+    plt.close()
 
 
 #######################################################################
@@ -47,6 +77,7 @@ dir_dnn_train = Utils.make_dir(sandbox + "/02-training/")
 # mimic_cxpert_label_data = MIMICCXR().join_mimic_labels(mimic_metadata_subset, cxpert_label_index_data, mimic_cxpert_label_name)
 # print("+ mimic_cxpert_label_index: ", mimic_cxpert_label_data.shape)
 #
+# ##############
 # ## Step-4: Read metadata index file and dataset image loader
 # mimic_index_dir = "/data/01_UB/CXR_Datasets/mimic-cxr-jpg/"
 # mimic_ss_metadata = pd.read_csv(os.path.join(dir_pp_index, "mimic_cxpert_label_index.csv"))
@@ -68,3 +99,34 @@ dir_dnn_train = Utils.make_dir(sandbox + "/02-training/")
 # print("+ train_df", train_df.shape)
 # print("+ vaid_df", valid_df.shape)
 # print("+ test_df", test_df.shape)
+
+######################################################################
+## DNN training
+
+## step 1: read train and valid sets
+DataIndex = collections.namedtuple('DataIndex', 'name img_paths labels labels_name')
+
+train_df = pd.read_csv(os.path.join(dir_pp_index, "mimic_train_set.csv"))
+valid_df = pd.read_csv(os.path.join(dir_pp_index, "mimic_valid_set.csv"))
+
+## Datasplit instances
+num_samples = 256
+train = DataIndex(name='train', img_paths=train_df["img_path"][:num_samples],
+                labels=train_df["label"][:num_samples], labels_name=train_df["label"][:num_samples])
+valid = DataIndex(name='valid', img_paths=valid_df["img_path"], labels=valid_df["label"],
+                                                labels_name=valid_df["label"])
+
+# print("train_df: ", train_df["img_path"].shape)
+# print("train_df: ", train_df["label"].shape)
+# print("valid_df: ", valid_df["img_path"].shape)
+# print("valid_df: ", valid_df["label"].shape)
+
+
+## step-2: Load the chest x-rays images in jpg
+images_folder = "/data/01_UB/CXR_Datasets/mimic-cxr-jpg/"
+train_set = Utils().image_loader(images_folder, train)
+valid_set = Utils().image_loader(images_folder, valid)
+
+
+history, model = DenseNET121().fit_binary_model(train_set, valid_set, dir_dnn_train, 5)
+displayLearningCurves(history)
